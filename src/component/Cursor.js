1f553592b671e378bb4c4954c9738a017d43a229
jriccrussel <jriccrussel@gmail.com>
2021-11-22 10:44:13 +0800

    complete cursor component

--- src/component/Cursor.js
+++ src/component/Cursor.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const Cursor = () => {
+
+    useEffect(() => {
+        let circle = document.querySelector(".circle")
+
+        window.addEventListener("mousemove", e => {
+            let xVal = e.clientX
+            let yVal = e.clientY
+
+            setTimeout(function(){
+                circle.style.top = `${yVal}px`
+                circle.style.left = `${xVal}px`
+            }, 100)
+        })
+
+
+    }, [])
+
     return (
         <>
-            cursor
+            <div className="circle"></div>
         </>
     )
 }
